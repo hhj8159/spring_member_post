508829930fd94f37496df6c7ff4c3ac6a31cbb6d
hhj8159 <hhj8159@gmail.com>
2024-12-18 18:24:20 +0900

    OptionalTests

---
+++ src/test/java/com/hjham/member_post/OptionalTests.java
@@ -0,0 +1,22 @@
+package com.hjham.member_post;
+
+import static org.mockito.ArgumentMatchers.nullable;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import lombok.extern.log4j.Log4j2;
+
+@SpringBootTest
+@Log4j2
+public class OptionalTests {
+  @Test
+  public void testOptional() {
+    Optional<String> o = Optional.ofNullable(null);
+    log.info(o);
+    log.info(o.isPresent());
+    log.info(o.get());
+  }
+}
